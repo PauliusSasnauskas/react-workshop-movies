4da1bbfc69a4d0cf2b108273ae45c6825abb0398
Paulius Sasnauskas <paulius.sasn@gmail.com>
2021-03-19 15:31:04 +0200

    change the color of the MovieItem if it is recommended

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -5,9 +5,15 @@ export default class MovieItem extends React.Component {
     render(){
         // 1. Use props (properties) to obtain movie parameters
         const movie = this.props.movie;
+        const isRecommended = this.props.recommended;
+
+        let bgColor = "transparent";
+        if (isRecommended){
+            bgColor = "#FFE050";
+        }
         
         // 2. Return a graphical representation of the MovieItem (some HTML)
-        return <div>
+        return <div style={{backgroundColor: bgColor}}>
             <img src={movie.img} />
             {movie.title} ({movie.year})
         </div>;
